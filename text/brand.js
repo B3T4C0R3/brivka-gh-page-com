b9fba2d8a98fb1eb0fcae33da06a805ce18228c2
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2023-01-06 20:25:28 +0000

    deploy: B3T4C0R3/brivka@4535d772b8136c9ac8ab59d60ec1c28370559f0b

---
+++ text/brand.js
@@ -0,0 +1,14 @@
+const brand = {
+  brivka: {
+    name: 'Brivka',
+    desc: 'Brivka',
+    prefix: 'Brivka',
+    footerText: 'Brivka',
+    logoText: 'Brivka',
+    projectName: 'Brivka',
+    url: 'b3t4c0r3.github.io',
+    img: '/static/images/logo.svg'
+  }
+}
+
+export default brand
